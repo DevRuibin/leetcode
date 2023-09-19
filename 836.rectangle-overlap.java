49a6fd95f302eb975888148f079bba61da8bebf4
Ruibin Zhang <ruibin.zhang021@icloud.com>
2023-09-19 14:49:08 +0200

    more file

--- 836.rectangle-overlap.java
+++ 836.rectangle-overlap.java
@@ -7,11 +7,18 @@
 // @lc code=start
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-       int count5 = 0;
-       int count10 = 0;
-       for (int i = 0; i < rec2.length; i++) {
-        
-       } 
+        int x1 = rec1[0], y1 = rec1[1];
+        int x2 = rec1[2], y2 = rec1[3];
+        int a1 = rec2[0], b1 = rec2[1];
+        int a2 = rec2[2], b2 = rec2[3];
+
+        return !(
+            (b1 >= y2) ||
+            (y1 >= b2) ||
+            (a1 >= x2) ||
+            (a2  <= x1)
+        );
+
     }
 }
 // @lc code=end
